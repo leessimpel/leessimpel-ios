fd8bf2c148114e88c1c6896e39884fc0bccc3f9d
Lucas Meijer <lucas@unity3d.com>
2023-02-23 15:00:33 +0100

    use real url

--- LeesSimpel/TextRecognition/BackendService.swift
+++ LeesSimpel/TextRecognition/BackendService.swift
@@ -2,8 +2,9 @@ import SwiftUI
 
 final class BackendService: ObservableObject {
     func summarizeTranscript(transcript: String) async throws -> QueryResponse {
-        let url = URL(string: "http://localhost:5182/summarize_text")!
-
+        //let url = URL(string: "http://localhost:5182/summarize_text")!
+        let url = URL(string: "https://simpelbackend.azurewebsites.net/summarize_text")!
+        
         var request = URLRequest(url: url)
         request.httpMethod = "POST";
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
